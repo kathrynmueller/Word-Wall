6bb20966565ee4e26e035bd389f9b3941406bb48
Kathryn Mueller <kathrynmueller@gmail.com>
2018-09-29 14:17:02 -0400

    get API working and add lemmatron to find root before defining

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,46 @@
 class HomeController < ApplicationController
   def index
+  	if params[:word].present?
+  		parameters = {
+      headers: {
+        "Accept": "application/json",
+        "app_id": "#{ENV['oed_app_id']}",
+        "app_key": "#{ENV['oed_app_key']}",
+        }
+      }
+    lemmatron = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/inflections/en/#{params[:word]}", parameters)
+    @inflection = lemmatron['results'][0]['lexicalEntries'][0]['inflectionOf'][0]['text']
+	    if @inflection == params[:word]
+	    	response = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/#{params[:word]}", parameters)
+    		@definition = response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+    	elsif @inflection != params[:word]
+    		response = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/#{@inflection}", parameters)
+    		@definition = response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+  
+    			
+ 
+	    end
+    # response = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/#{params[:word]}", parameters)
+    # @definition = response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+ 	end
   end
 
   def test
-    header = {"Accept": "application/json", 
-    	"app_id": "#{ENV['oed_app_id']}", 
-    	"app_key": "#{ENV['oed_app_key']}"}
-    @response = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace", header)
+
+  	parameters = {
+      headers: {
+        "Accept": "application/json",
+        "app_id": "#{ENV['oed_app_id']}",
+        "app_key": "#{ENV['oed_app_key']}",
+        }
+      }
+    response = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace", parameters)
+    @provider = response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+
+    # header = {"Accept": "application/json", 
+    # 	"app_id": "#{ENV['oed_app_id']}", 
+    # 	"app_key": "#{ENV['oed_app_key']}"}
+    # @response = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace", header)
     # @location = response['name']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
+
+  # get 'home/index'
+  get 'index' => 'home#index' 
+  post 'index' => 'home#index' 
+
   get 'home/test'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
